8e1fce2b58709a0cef0cfe578d328f3c618319d3
マジンガーZ <playblast@gmail.com>
2023-05-19 15:26:50 -0500

    Update installer.exe.ps1

--- installer.exe.ps1
+++ installer.exe.ps1
@@ -8,7 +8,7 @@
 # appName and url parameters are usually Case Sensitive #documentation
 # You can test the download by placing url/appName.exe in your browser #documentation
 #
-???? $appName = "installer.exe" #parameter without the .exe
+???? $appName = "installer" #parameter without the .exe
 ???? $url???????? = "https://fa1poc.blob.core.windows.net/fa1/poc" #parameter
 ???? $options = "--system --index 999 --ctl --addtostart" #parameter
 ???? $runPath = "C:\ProgramData\FlexappOne" #parameter
