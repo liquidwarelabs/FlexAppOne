d594c77adbceec4f4d7212e358f83aaf7114f4e4
David Bieneman <david.bieneman@gmail.com>
2024-05-21 16:51:14 -0500

    Update 01 - 3D Pinball Win95-98 - Game.exe.ps1

--- 01 - 3D Pinball Win95-98 - Game.exe.ps1
+++ 01 - 3D Pinball Win95-98 - Game.exe.ps1
@@ -11,41 +11,61 @@
 # appName and url parameters are usually Case Sensitive    #documentation
 # You can test the download by placing url/appName.exe in your browser #documentation
 
-#
 $appName = "01 - 3D Pinball Win95-98 - Game"               #parameter without the .exe
+$installer = "installer"                                   #parameter without the .exe
+$instoptions = "--install"                                 #parameter
+$instcheck = "C:\Program Files\ProfileUnity\FlexApp\ContainerService\x64\VirtFsService.exe"  #parameter
 $url     = "https://fa1poc.blob.core.windows.net/fa1/poc"  #parameter
 $options = "--system --index 999 --ctl --addtostart"       #parameter
 $runPath = "C:\ProgramData\FlexAppOne"                     #parameter
-#
-# Normally you do not edit past here
 
-#Create FlexApp One download location - runPath
-If(!(test-path -PathType container $runPath))
-{
+# Create FlexApp One download location - runPath
+If (!(Test-Path -PathType Container $runPath)) {
     New-Item -ItemType Directory -Path $runPath
 }
 
-# Download required files for the app
+# Check if VirtFsService.exe exists
+If (!(Test-Path -Path $instcheck)) {
+    Write-Output "File not found: $instcheck. Downloading and installing $installer."
+
+    # Download installer
+    Write-Output "Downloading latest version of '$installer' from $url"
+    $startTime = Get-Date
+
+    # BitsTransfer module
+    Import-Module BitsTransfer
+    Start-BitsTransfer -Source "$url/$installer.exe" -Destination "$runPath\$installer.exe"
+    Write-Output "Time taken: $((Get-Date).Subtract($startTime).Seconds) second(s)"
+    Write-Output "Installer downloaded."
+
+    # Run installer with options
+    Write-Output "Running installer."
+    Start-Process -FilePath "$runPath\$installer.exe" -ArgumentList "$instoptions" -NoNewWindow -Wait
+    Write-Output "Installer $installer.exe was run with $instoptions."
+} else {
+    Write-Output "File already exists: $instcheck"
+}
+
+# Download the FlexApp
 Write-Output "Downloading latest version of '$appName' from $url"
 $startTime = Get-Date
 
 # BitsTransfer module
 Import-Module BitsTransfer
-Start-BitsTransfer -Source $url/$appName.exe -Destination "$runPath\$appName.exe"
+Start-BitsTransfer -Source "$url/$appName.exe" -Destination "$runPath\$appName.exe"
 Write-Output "Time taken: $((Get-Date).Subtract($startTime).Seconds) second(s)"
-Write-host "New application downloaded."
+Write-Output "New application downloaded."
 
 # Stop Clean the app before running
-Write-host "Stop Clean app."
+Write-Output "Stop Clean app."
 Start-Process -FilePath "$runPath\$appName.exe" -ArgumentList "--stop --clean" -Wait
-Write-host "FlexApp One $appName.exe, was run with --stop --clean."
+Write-Output "FlexApp One $appName.exe, was run with --stop --clean."
 
 # Run FlexApp with Options
-Write-host "Loading new app."
+Write-Output "Loading new app."
 Start-Process -FilePath "$runPath\$appName.exe" -ArgumentList "$options"
-Write-host "FlexApp One $appName.exe, was run with $options."
+Write-Output "FlexApp One $appName.exe, was run with $options."
 
-# Comment out everything below if you do not want to use the task scheduler to run the FlexApp.exe $options on boot
 # Remove existing scheduled task with the same name
 try {
     Unregister-ScheduledTask -TaskName "${appName}.exe" -Confirm:$false -PassThru:$true
@@ -61,4 +81,4 @@ $taskSettings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit (New-TimeSpan -
 $task = New-ScheduledTask -Action $taskAction -Principal $taskPrincipal -Trigger $taskTrigger -Settings $taskSettings
 Register-ScheduledTask -TaskName "${appName}.exe" -InputObject $task
 
-Write-host "FlexApp One $appName.exe, was run with $options."
+Write-Output "FlexApp One $appName.exe, was run with $options."
