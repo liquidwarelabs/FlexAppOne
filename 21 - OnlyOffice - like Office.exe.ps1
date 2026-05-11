0405575ab9587a1fe400d93152bc2e02cc3c785c
David Bieneman <david.bieneman@gmail.com>
2026-05-11 00:39:07 -0500

    Update comments and parameters in OnlyOffice script

--- 21 - OnlyOffice - like Office.exe.ps1
+++ 21 - OnlyOffice - like Office.exe.ps1
@@ -5,6 +5,8 @@
 #description: 21 - OnlyOffice - like Office via Liquidware FlexApp One. There are many ways to centrally 'stream' or 'deploy' FlexApp / FlexApp One.
 #tags: Liquidware FlexApp
 #execution mode: Combined
+#Type: app
+#Category: FlexApp Applications
 
 # Only edit the following section                          #documentation
 # For appName, do not add .exe                             #documentation
@@ -17,7 +19,7 @@ $installer = "installer"                                   #parameter without th
 $instoptions = "--install"                                 #parameter
 $instcheck = "C:\Program Files\ProfileUnity\FlexApp\ContainerService\x64\VirtFsService.exe"  #parameter
 $url     = "https://fa1poc.blob.core.windows.net/fa1/poc"  #parameter
-$options = "--system --index 999 --ctl --addtostart"       #parameter
+$options = "--system --outofband --index 999 --ctl --addtostart"       #parameter
 $runPath = "C:\ProgramData\FlexAppOne"                     #parameter
 # Normally you do not edit past here
 
